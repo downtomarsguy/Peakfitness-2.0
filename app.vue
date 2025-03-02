e7cc7da5cc7dbdbdb369411da427d07f9407e449
downtomarsguy <theonesilverest@gmail.com>
2025-03-02 12:23:24 +0530

    Hero Section Added

--- app.vue
+++ app.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="h-screen flex flex-col">
     <Navbar />
-    
-    <div class="flex-1 flex justify-center items-center">
-    </div>
+
+      <HeroSection />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
+import Navbar from "./components/Navbar.vue";
+import HeroSection from "./components/HeroSection.vue";
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    HeroSection
   },
 };
 </script>
---
+++ components/HeroSection.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="relative h-screen flex items-center justify-between p-8 bg-black overflow-hidden">
+      <div class="max-w-lg space-y-6">
+        <h1 class="text-4xl font-extrabold text-white">
+          Build Perfect For Good And Healthy Life
+        </h1>
+        <button class="px-6 py-3 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600">
+          Get Started
+        </button>
+      </div>
+  
+      <div class="absolute bottom-0 right-0 w-1/3 h-full">
+        <img src="@/assets/main4.jpg" alt="Hero Image" class="w-full h-full object-cover rounded-lg" />
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HeroSection',
+  };
+  </script>
+  
+  <style>
+  @import "tailwindcss";
+  </style>
+  
